33ae78fe1ea6f78f14690eaaa08786458d260a08
Sankara Rameswaran <hello@sankara.me>
2022-03-11 17:29:04 -0800

    Optimizing order create and load
    There's no need to load all the products and iterate through
    them to get product details or to check for product id.

--- gateway/gateway/service.py
+++ gateway/gateway/service.py
@@ -93,9 +93,6 @@ class GatewayService(object):
         # raise``OrderNotFound``
         order = self.orders_rpc.get_order(order_id)
 
-        # Retrieve all products from the products service
-        product_map = {prod['id']: prod for prod in self.products_rpc.list()}
-
         # get the configured image root
         image_root = config['PRODUCT_IMAGE_ROOT']
 
@@ -103,7 +100,7 @@ class GatewayService(object):
         for item in order['order_details']:
             product_id = item['product_id']
 
-            item['product'] = product_map[product_id]
+            item['product'] = self.products_rpc.get(product_id)
             # Construct an image url.
             item['image'] = '{}/{}.jpg'.format(image_root, product_id)
 
@@ -157,9 +154,8 @@ class GatewayService(object):
 
     def _create_order(self, order_data):
         # check order product ids are valid
-        valid_product_ids = {prod['id'] for prod in self.products_rpc.list()}
         for item in order_data['order_details']:
-            if item['product_id'] not in valid_product_ids:
+            if not self.products_rpc.get(item['product_id']):
                 raise ProductNotFound(
                     "Product Id {}".format(item['product_id'])
                 )
@@ -172,3 +168,4 @@ class GatewayService(object):
             serialized_data['order_details']
         )
         return result['id']
+
